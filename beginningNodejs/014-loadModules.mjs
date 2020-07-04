91e1fcce2769bbd44e94d9996e765f8b5f984c52
Vagacoder <qiruihu@gmail.com>
2020-07-03 18:33:10 -0700

    7/3 working on loading modules

---
+++ beginningNodejs/014-loadModules.mjs
@@ -0,0 +1,11 @@
+/*
+ * node js load modules 
+ */
+
+// * firebase-tools is installed globally, it is not meant to using import/require
+// import firebase from 'firebase-tools';
+
+import _ from 'underscore';
+
+console.log('014');
+console.log('underscore version:', _.VERSION);
