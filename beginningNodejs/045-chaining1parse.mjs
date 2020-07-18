14d1e2b125d0c20737f37d8f3c454c5b7bf3bb56
Vagacoder <qiruihu@gmail.com>
2020-07-17 18:39:58 -0700

    7/17 chaining 1

---
+++ beginningNodejs/045-chaining1parse.mjs
@@ -0,0 +1,46 @@
+/*
+ * node js, middleware chaining 1, parse JSON 
+ */
+
+import connect from 'connect';
+
+// * middleware parsing json in request
+function parseJSON(req, res, next){
+    // * if header shows its json, parse it, or pass to next middleware
+    if(req.headers['content-type'] === 'application/json'){
+        let readData = '';
+        
+        // * read stream
+        req.on('readable', function(){
+            if(req.read){
+                readData += req.read();
+            }
+        });
+
+        // * once read whole data, parse it to json object and attach to req.body
+        req.on('end', function(){
+            try{
+                console.log(readData);
+                readData = readData.substring(0, readData.length-4);
+                console.log(readData);
+                req.body = JSON.parse(readData);
+            }catch(err){
+                console.error(err);
+            }
+            // * whether attach or not, pass it to next middleware
+            next();
+        })
+    }else{
+        next();
+    }
+}
+
+const app = connect()
+.use(parseJSON)
+.use(function (req, res){
+    if(req.body){
+        res.end('JSON parsed!, value is: ' + JSON.stringify(req.body));
+    }else{
+        res.end('no JSON detected!');
+    }
+}).listen(3000);
